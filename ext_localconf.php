af58bed5bbdb4d324b9d4072f5b7e1f8480ee25d
Stefano Kowalke <blueduck@mailbox.org>
2018-03-16 19:27:50 +0100

    In order to make the extension available for non-composer based TYPO3 installations
    * Add a PHAR Builder to the repository. This is a temporary solution. The PHAR builder should be installed globally on the system but
    due to version mismatch, this will not work at the moment.
    
    * Add a composer command to the script section which clones the latest version of PHP Debugbar, installs all its dependencies and creates a PHAR file out of it.
    This is necessary because the ExtensionManager is not able to do this by its own on non-composer based installations.
    
    * Remove the cloned repo after PHAR file is built
    
    * When installing the extension via the Extension Manager, the PHAR is autoloaded.

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,7 @@
 defined('TYPO3_MODE') or die('Access denied.');
 
 $extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY);
+require $extensionPath . 'Resources/Private/ExtensionArtifacts/autoload.php';
 
 if (TYPO3_MODE === 'FE') {
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['connectToDB'][] =
