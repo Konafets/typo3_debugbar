744a280fae571baa19eb01e230332ad30fd7f3d5
Stefan Kowalke <blueduck@mailbox.org>
2018-03-18 15:12:13 +0000

    Apply fixes from StyleCI

--- Resources/Private/ExtensionArtifacts/autoload.php
+++ Resources/Private/ExtensionArtifacts/autoload.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 return (function () {
     $pharFile = __DIR__ . '/../../../Libraries/debugbar.phar';
--- helpers.php
+++ helpers.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types=1);
 use Konafets\Typo3Debugbar\Typo3DebugBar;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
