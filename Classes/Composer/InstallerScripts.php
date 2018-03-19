0f9cb7b0f76c5dc7fa9997c53b68bfed57f90db3
Stefano Kowalke <blueduck@mailbox.org>
2018-03-19 20:28:05 +0100

    Add support for Travis

---
+++ Classes/Composer/InstallerScripts.php
@@ -0,0 +1,32 @@
+<?php
+namespace Konafets\Typo3Debugbar\Composer;
+
+use Composer\Script\Event;
+use Composer\Semver\Constraint\EmptyConstraint;
+use Symfony\Component\Console\Exception\RuntimeException;
+
+/**
+ * Scripts executed on composer build time
+ *
+ * This file is taken from helhum/typo3_console. Thanks!
+ */
+class InstallerScripts
+{
+    /**
+     * @param Event $event
+     * @internal
+     * @throws RuntimeException
+     */
+    public static function setVersion(Event $event)
+    {
+        $version = $event->getArguments()[0];
+        if (!preg_match('/\d+\.\d+\.\d+/', $version)) {
+            throw new RuntimeException('No valid version number provided!', 1468672604);
+        }
+
+        $extEmConfFile = __DIR__ . '/../../ext_emconf.php';
+        $content = file_get_contents($extEmConfFile);
+        $content = preg_replace('/(\'version\' => )\'\d+\.\d+\.\d+/', '$1\'' . $version, $content);
+        file_put_contents($extEmConfFile, $content);
+    }
+}
